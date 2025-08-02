46b2e183f520c7a615f1070ae51b6244c44a5e71
AJ Roberts <ajroberts2751@gmail.com>
2025-08-01 17:30:47 -0700

    Frog now can crouch, made an updated character movement script to do this, also updated player prefab

---
+++ Assets/Scripts/FrogCharacterMovement.cs
@@ -0,0 +1,46 @@
+using CharacterMovement;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FrogCharacterMovement : CharacterMovement2D
+{
+    [Header("Crouching")]
+    [SerializeField] protected float _crouchHeight;
+    [SerializeField] protected Sprite _crouchedSprite;
+    [SerializeField] protected float _spriteCrouchedScale = 1.5f;
+
+    protected float _baseHeight;
+    protected float _baseSpriteScale;
+    protected Sprite _baseSprite;
+    protected SpriteRenderer _spriteRenderer;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _baseHeight = Height;
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _baseSprite = _spriteRenderer.sprite;
+        _baseSpriteScale = _spriteRenderer.transform.localScale.y;
+    }
+
+    public void OnCrouch(InputValue value)
+    {
+        _spriteRenderer.sprite = _crouchedSprite;
+
+
+        //Height = _crouchHeight;
+        CapsuleCollider.size = new Vector2(1, _crouchHeight);
+        //_spriteRenderer.transform.localScale.Set(1,_spriteCrouchedScale,1);
+    }
+
+    public void OnUnCrouch(InputValue value)
+    {
+        _spriteRenderer.sprite = _baseSprite;
+        
+        
+        //Height = _baseHeight;
+        CapsuleCollider.size = new Vector2(1, _baseHeight);
+        //_spriteRenderer.transform.localScale.Set(1,_baseSpriteScale,1);
+    }
+}
